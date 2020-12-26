1523dde44efe2be14c0eb1af29d58157f8172883
Daniel Chien <tanchien@fb.com>
2020-12-25 17:11:01 -0800

    add home page

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+// @format
+import React, {useState} from 'react'
 
-function App() {
+import {
+  MenuItem,
+  FormControl,
+  FormGroup,
+  Paper,
+  Button,
+  TextField,
+  Grid
+} from '@material-ui/core'
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100vw',
+    height: '100vh'
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 350,
+    height: 150,
+    padding: theme.spacing(2)
+  },
+  rowContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: theme.spacing(1),
+    width: 250,
+  },
+  rowInput: {
+    width: 140,
+  }
+}));
+
+const App = () => {
+  const classes = useStyles();
+  const [boardIndex, setBoardIndex] = useState();
+  const [gameID, setGameID] = useState();
+
+  const boards = ['預女獵白', '狼王守衛'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid
+      className={classes.root}
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      <Paper
+        className={classes.container}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <FormControl>
+          <FormGroup className={classes.rowContainer} row>
+            <TextField
+              className={classes.rowInput}
+              id="board-select"
+              select
+              label="板子"
+              value={boardIndex}
+              onChange={(e)=> setBoardIndex(e.target.value)}
+            >
+              {boards.map((board, idx) => (
+                <MenuItem key={idx} value={idx}>
+                  {board}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button variant="outlined" color="primary">
+              Create
+            </Button>
+          </FormGroup>
+          <FormGroup className={classes.rowContainer} row>
+            <TextField
+              autoComplete="off"
+              className={classes.rowInput}
+              value={gameID}
+              onChange={(e) => setGameID(e.target.value)}
+              id="game-id"
+              label="遊戲 ID" />
+            <Button variant="outlined" color="secondary">
+              Join
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </Paper>
+    </Grid>
+  )
 }
 
 export default App;
